e0ec443c2396757cda1155b2b4080c264dd94bb9
Richard Nguyen <richard.nguyen@guardian.co.uk>
2014-01-28 11:11:08 +0000

    Update grunt tasks with new dir structure, drop dataUri task

--- gruntfile.js
+++ gruntfile.js
@@ -3,30 +3,13 @@ module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
 
-        // concat: {
-        //     build: {
-        //         src: [
-        //             '_src/scripts/libs/*.js',
-        //             '_src/scripts/scripts.js'
-        //         ],
-        //         dest: 'assets/scripts/scripts.js',
-        //     }
-        // },
-        // 
-        // uglify: {
-        //     build: {
-        //         src: 'assets/scripts/scripts.js',
-        //         dest: 'assets/scripts/scripts.min.js'
-        //     }
-        // },
-
         imagemin: {
             build: {
                 files: [{
                     expand: true,
-                    cwd: '_src/images/',
+                    cwd: 'jekyll/_src/images/',
                     src: ['**/*.{png,jpg}'],
-                    dest: 'assets/images/'
+                    dest: 'jekyll/assets/images/'
                 }]
             }
         },
@@ -35,9 +18,9 @@ module.exports = function(grunt) {
             build: {
                 files: [{
                     expand: true,
-                    cwd: '_src/images/',
+                    cwd: 'jekyll/_src/images/',
                     src: ['**/*.svg'],
-                    dest: 'assets/images/'
+                    dest: 'jekyll/assets/images/'
                 }]
             }
         },
@@ -49,19 +32,7 @@ module.exports = function(grunt) {
                     loadPath: 'bower_components'
                 },
                 files: {
-                    'assets/styles/styles.css': '_src/styles/styles.scss'
-                }
-            }
-        },
-        
-        dataUri: {
-            build: {
-                src: ['assets/styles/*.css'],
-                dest: 'assets/styles',
-                options: {
-                    target: ['assets/images/embed/*.*'],
-                    fixDirLevel: true,
-                    baseDir: './'
+                    'jekyll/assets/styles/styles.css': 'jekyll/_src/styles/styles.scss'
                 }
             }
         },
@@ -72,35 +43,28 @@ module.exports = function(grunt) {
                     report: 'gzip',
                 },
                 files: {
-                    'assets/styles/styles.min.css': 'assets/styles/styles.css'
+                    'jekyll/assets/styles/styles.min.css': 'jekyll/assets/styles/styles.css'
                 }
             }
         },
 
         watch: {
-            // scripts: {
-            //     files: ['_src/scripts/*.js'],
-            //     tasks: ['concat', 'uglify'],
-            //     options: {
-            //         spawn: false,
-            //     },
-            // },
             images: {
-                files: ['_src/images/**/*.{png,jpg}'],
+                files: ['jekyll/_src/images/**/*.{png,jpg}'],
                 tasks: ['imagemin'],
                 options: {
                     spawn: false,
                 },
             },
             vectors: {
-                files: ['_src/images/**/*.svg'],
+                files: ['jekyll/_src/images/**/*.svg'],
                 tasks: ['svgmin'],
                 options: {
                     spawn: false,
                 },
             },
             styles: {
-                files: ['_src/styles/**/*.scss'],
+                files: ['jekyll/_src/styles/**/*.scss'],
                 tasks: ['sass', 'dataUri', 'cssmin'],
                 options: {
                     spawn: false,
@@ -109,14 +73,11 @@ module.exports = function(grunt) {
         }
     });
 
-    // grunt.loadNpmTasks('grunt-contrib-concat');
-    // grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-imagemin');
     grunt.loadNpmTasks('grunt-contrib-sass');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-svgmin');
-    grunt.loadNpmTasks("grunt-data-uri");
 
-    grunt.registerTask('default', ['imagemin', 'svgmin', 'sass', 'dataUri', 'cssmin']);
+    grunt.registerTask('default', ['imagemin', 'svgmin', 'sass', 'cssmin']);
 };
