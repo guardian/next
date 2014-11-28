81c66bf0c3778555c384d5fe739e711718d471a9
Matt Osborn <matthew.osborn@theguardian.com>
2014-11-28 18:12:06 +0000

    more redesign

--- gruntfile.js
+++ gruntfile.js
@@ -110,5 +110,5 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-jekyll');
     grunt.loadNpmTasks('grunt-bower-task');
 
-    grunt.registerTask('default', ['bower', 'imagemin', 'svgmin', 'sass', 'cssmin', 'jekyll']);
+    grunt.registerTask('default', ['bower', 'imagemin', 'svgmin', 'sass', 'cssmin', 'copy', 'jekyll']);
 };
